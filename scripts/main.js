c2f8878e173038cf271810057b1de9a8ba6d0588
DMartinez-Van <david.antonio.martinez@gmail.com>
2025-03-18 23:26:13 -0700

    add alternative to displayEvents function. Invovles a where call to db instead of using references within user's doc.

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,13 @@
 //     firebase.auth().onAuthStateChanged(user => {
 //         // Check if a user is signed in:
 //         if (user) {
-//             // Do something for the currently logged-in user here: 
+//             // Do something for the currently logged-in user here:
 //             console.log(user.uid); //print the uid in the browser console
 //             console.log(user.displayName);  //print the user name in the browser console
 //             userName = user.displayName;
 
 //             //method #1:  insert with JS
-//             document.getElementById("name-goes-here").innerText = userName;    
+//             document.getElementById("name-goes-here").innerText = userName;
 
 //             //method #2:  insert using jquery
 //             //$("#name-goes-here").text(userName); //using jquery
@@ -25,58 +25,101 @@
 // getNameFromAuth(); //run the function
 
 function insertNameFromFirestore() {
-    // Check if the user is logged in:
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
-            currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
-            currentUser.get().then(userDoc => {
-                // Get the user name
-                let userName = userDoc.data().name;
-                console.log(userName);
-                //$("#name-goes-here").text(userName); // jQuery
-                document.getElementById("name-goes-here").innerText = userName;
+  // Check if the user is logged in:
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+      currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+      currentUser.get().then((userDoc) => {
+        // Get the user name
+        let userName = userDoc.data().name;
+        console.log(userName);
+        //$("#name-goes-here").text(userName); // jQuery
+        document.getElementById("name-goes-here").innerText = userName;
 
-                displayEvents(user.uid);
-            })
-        } else {
-            console.log("No user is logged in."); // Log a message when no user is logged in
-        }
-    })
+        displayEvents(user.uid);
+      });
+    } else {
+      console.log("No user is logged in."); // Log a message when no user is logged in
+    }
+  });
 }
 insertNameFromFirestore();
 
-function displayEvents(userID){     //Populate Event Gallery
-    let template = document.getElementById("event-card-template");
-    db.collection("users").doc(userID).collection("events").get()
-        .then(allEvents => {
-            allEvents.forEach(doc => {
-                if (allEvents.size > 1){    //if more events than the init are present, create event cards
-                    eventInfo = db.collection("events").doc(doc.id);
-                    eventInfo.get().then(eventDoc => {
-                    if(eventDoc.id != "init"){
-                        let name = eventDoc.data().name;
-                        let date = eventDoc.data().date;
-                        let time = eventDoc.data().time;
-                        let location = eventDoc.data().location;
-                        let newCard = template.content.cloneNode(true);
+function displayEvents(userID) {
+  //Populate Event Gallery
+  let template = document.getElementById("event-card-template");
+  db.collection("users")
+    .doc(userID)
+    .collection("events")
+    .get()
+    .then((allEvents) => {
+      allEvents.forEach((doc) => {
+        if (allEvents.size > 1) {
+          //if more events than the init are present, create event cards
+          eventInfo = db.collection("events").doc(doc.id);
+          eventInfo.get().then((eventDoc) => {
+            if (eventDoc.id != "init") {
+              let name = eventDoc.data().name;
+              let date = eventDoc.data().date;
+              let time = eventDoc.data().time;
+              let location = eventDoc.data().location;
+              let newCard = template.content.cloneNode(true);
 
-                        newCard.querySelector('.card-title').innerHTML = name;
-                        newCard.querySelector('.event-date').innerHTML = date;
-                        newCard.querySelector('.event-time').innerHTML = time;
-                        newCard.querySelector('.event-location').innerHTML = location;
-                        console.log("Event ID: " + eventDoc.id);
-                        newCard.querySelector('a').href = "event-page.html?docID=" + eventDoc.id;
+              newCard.querySelector(".card-title").innerHTML = name;
+              newCard.querySelector(".event-date").innerHTML = date;
+              newCard.querySelector(".event-time").innerHTML = time;
+              newCard.querySelector(".event-location").innerHTML = location;
+              console.log("Event ID: " + eventDoc.id);
+              newCard.querySelector("a").href =
+                "event-page.html?docID=" + eventDoc.id;
 
-                        document.getElementById("event-gallery").appendChild(newCard);
-                    }
-                    
-                })
-
-                }else{
-                    console.log($('#event-gallery').load('./text/noEvents.html')); //if user has no events, prompt them to
-                }
-                
-            })
-        })
+              document.getElementById("event-gallery").appendChild(newCard);
+            }
+          });
+        } else {
+          console.log($("#event-gallery").load("./text/noEvents.html")); //if user has no events, prompt them to
+        }
+      });
+    });
 }
+
+// function displayEventsVer2(userID) {
+//   //Populate Event Gallery
+//   let template = document.getElementById("event-card-template");
+//   console.log("User ID: " + userID);
+//   db.collection("events")
+//     .where("hostId", "==", userID)
+//     .get()
+//     .then((allEvents) => {
+//       let myEvents = allEvents.docs;
+//       console.log("My Events: " + myEvents);
+//       myEvents.forEach((doc) => {
+//         if (allEvents.size > 1) {
+//           //if more events than the init are present, create event cards
+//           eventInfo = db.collection("events").doc(doc.id);
+//           eventInfo.get().then((eventDoc) => {
+//             if (eventDoc.id != "init") {
+//               let name = eventDoc.data().name;
+//               let date = eventDoc.data().date;
+//               let time = eventDoc.data().time;
+//               let location = eventDoc.data().location;
+//               let newCard = template.content.cloneNode(true);
+
+//               newCard.querySelector(".card-title").innerHTML = name;
+//               newCard.querySelector(".event-date").innerHTML = date;
+//               newCard.querySelector(".event-time").innerHTML = time;
+//               newCard.querySelector(".event-location").innerHTML = location;
+//               console.log("Event ID: " + eventDoc.id);
+//               newCard.querySelector("a").href =
+//                 "event-page.html?docID=" + eventDoc.id;
+
+//               document.getElementById("event-gallery").appendChild(newCard);
+//             }
+//           });
+//         } else {
+//           console.log($("#event-gallery").load("./text/noEvents.html")); //if user has no events, prompt them to
+//         }
+//       });
+//     });
+// }
