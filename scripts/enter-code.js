e165bba323e2be67014a832efd80667f61e34520
DMartinez-Van <david.antonio.martinez@gmail.com>
2025-03-22 14:12:35 -0700

    add skeleton for enter-code js fie

---
+++ scripts/enter-code.js
@@ -0,0 +1,12 @@
+// taget join btn
+console.log("AM I LOADED?");
+const joinBtn = document.getElementById("join-btn");
+console.log("joinBtn: ", joinBtn);
+
+joinBtn.addEventListener("click", (e) => {
+  //   e.preventDefault();
+  const code = document.getElementById("inviteCode").value;
+  console.log("JOINING EVENT");
+
+  // redirect to a specific event page with the code from "code" variable
+});
