dcd1af060e275b2ba8f587540a7ce26d618a02a9
DMartinez-Van <david.antonio.martinez@gmail.com>
2025-03-23 13:51:16 -0700

    adjust functionality of enter-code.js so that a correct code redirects a user to the event page

--- scripts/enter-code.js
+++ scripts/enter-code.js
@@ -2,41 +2,27 @@ document.getElementById("join-btn").addEventListener("click", (e) => {
   const code = document.getElementById("inviteCode").value;
 
   if (new RegExp("^[A-Z]{5}$").test(code)) {
-    console.log("JOINING EVENT");
+    console.log("Acceptable code format. Looking for event with code: ", code);
 
-    // redirect to a specific event page with the code from "code" variable
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        userID = user.uid;
-        db.collection("events")
-          .where("hostId", "==", userID)
-          .get()
-          .then((allEvents) => {
-            if (!allEvents.empty) {
-              allEvents.forEach((event) => {
-                if (event.data().eventCode === code) {
-                  console.log("Event found:", event.id);
-
-                  // Redirect to the event page with the event ID
-                  window.location.href = 'event-page.html?docID=' + event.id;
-                } else {
-                  console.log("No event found with the provided code.");
-                  alert("No event found with the provided code.");
-                  // Optionally, display a message to the user
-                }
-              });
-            }
-          })
-          .catch((error) => {
-            console.error("Error fetching events:", error);
-          });
-
-      } else {
-        console.log("No user is logged in.");
-      }
-    });
+    db.collection("events")
+      .where("eventCode", "==", code)
+      .get()
+      .then((event) => {
+        // Firebase always gets us a query result, even if the query is empty. Thus we need to check if the query is empty.
+        // And throw an alert if it is.
+        if (event.empty) {
+          alert("No matching event found.");
+          console.log("No matching event found.");
+          return;
+        }
+        console.log("event found -> ", event.docs[0].id);
+        window.location.href = "event-page.html?docID=" + event.docs[0].id;
+      })
+      .catch((error) => {
+        // The catch is fired if there is an error with the connection to the db (I think)
+        console.error("Error fetching events:", error);
+      });
   } else {
     alert("Invalid code. Please enter a valid code.");
   }
-
 });
