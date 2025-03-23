6eeb3bf402ebd9651099dd8b30b3e7d043990bbe
DMartinez-Van <david.antonio.martinez@gmail.com>
2025-03-23 14:52:49 -0700

    adjust view event page to match figma

---
+++ scripts/create-event.js
@@ -0,0 +1,133 @@
+const submitBtn = document.getElementById("submit-event");
+const submitMobileBtn = document.getElementById("submit-event-mobile");
+
+function addEvent(e) {
+  console.log("addEvent function called");
+  const auth = firebase.auth();
+
+  let eventName = document.getElementById("event-name");
+  let eventDetails = document.getElementById("event-details");
+
+  let selectedDay = selectedDateFormArray;
+  let selectedMonth = months[currentMonth];
+  let selectedYear = currentYear;
+  let selectedTime = document.querySelector('input[name="time"]:checked').value;
+  let location = document.getElementById("event-location");
+  let eventCode = generateCode();
+  let hostID = db.collection("users").doc(auth.currentUser.uid);
+
+  console.log("selectedDay: ", selectedDay);
+
+  let event = {
+    name: eventName.value,
+    description: eventDetails.value,
+    dateYear: selectedYear,
+    dateMonth: selectedMonth,
+    dateDay: selectedDay,
+    selectedTime: selectedTime,
+    date: `${selectedMonth} ${selectedDay}, ${selectedYear}`,
+    location: location.value,
+    planner: hostID,
+    eventCode: eventCode,
+    dateConfirmed: false, // For use when confirmed the event (and provide highlight onto events)
+  };
+
+  function generateCode() {
+    let codeString = "";
+    for (let i = 0; i < 5; i++) {
+      codeString += String.fromCharCode(65 + Math.floor(Math.random() * 25));
+    }
+    return codeString;
+  }
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      // First get the user's name to add to the event
+      db.collection("users")
+        .doc(user.uid)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            event.host = doc.data().name;
+            event.hostId = user.uid;
+
+            // Then add the event to the events collection
+            db.collection("events")
+              .add(event)
+              .then((docRef) => {
+                console.log("Event added with ID: ", docRef.id);
+
+                // Add a reference to the created event in the user's subcollection
+                db.collection("users")
+                  .doc(user.uid)
+                  .collection("events")
+                  .doc(docRef.id)
+                  .set({
+                    eventRef: docRef,
+                  })
+                  .then(() => {
+                    console.log(
+                      "Event reference added to user's subcollection"
+                    );
+                  })
+                  .catch((error) => {
+                    console.error(
+                      "Error adding event reference to user's subcollection: ",
+                      error
+                    );
+                  });
+              });
+          }
+        })
+        .catch((error) => {
+          console.log("EVENT NOT ADDED");
+          console.error("Error adding event: ", error);
+        });
+    }
+  });
+}
+
+document.getElementById("time-am").addEventListener("click", () => {
+  swapActiveTime();
+});
+document.getElementById("time-pm").addEventListener("click", () => {
+  swapActiveTime();
+});
+
+function swapActiveTime() {
+  let timeAmBtn = document.getElementById("time-am");
+  let amInput = document.getElementById("time-am-input");
+  let timePmBtn = document.getElementById("time-pm");
+  let pmInput = document.getElementById("time-pm-input");
+  if (timeAmBtn.classList.contains("btn-active")) {
+    // remove active from AM and add to PM
+    timeAmBtn.classList.add("btn-inactive");
+    timeAmBtn.classList.remove("btn-active");
+    amInput.checked = false;
+
+    timePmBtn.classList.add("btn-active");
+    timePmBtn.classList.remove("btn-inactive");
+    pmInput.checked = true;
+    console.log("time-am-input: ", timeAmBtn);
+  } else {
+    // remove active from PM and add to AM
+    timeAmBtn.classList.add("btn-active");
+    timeAmBtn.classList.remove("btn-inactive");
+    amInput.checked = true;
+
+    timePmBtn.classList.add("btn-inactive");
+    timePmBtn.classList.remove("btn-active");
+    pmInput.checked = false;
+  }
+}
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addEvent(e);
+});
+
+submitMobileBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("submitMobileBtn clicked");
+  addEvent(e);
+});
--- scripts/event-page.js
+++ scripts/event-page.js
@@ -16,16 +16,23 @@ function displayEventInfo() {
       eventTime = doc.data().time;
       eventDescription = doc.data().description;
       eventCode = doc.data().eventCode;
+      selectedTime = doc.data().selectedTime;
 
       // Populate html with info
       document.getElementById("event-title").innerHTML = eventName;
       document.getElementById("event-date").innerHTML = "Date:	" + eventDate;
-      document.getElementById("event-time").innerHTML = "Time:	" + eventTime;
+      // document.getElementById("event-time").innerHTML = "Time:	" + eventTime;
+      document.getElementById("event-time").innerHTML = "Time:	" + selectedTime;
       document.getElementById("event-location").innerHTML =
         "Location:	" + eventLocation;
       document.getElementById("description").innerHTML = eventDescription;
       document.getElementById("eventCode").style.visibility = "hidden";
       document.getElementById("eventCode").innerHTML = eventCode;
+
+      // select dates which match the eventDate list from the database and hightlight them
+      let eventDateList = eventDate.split(" "); // split the date string into an array
+      let eventDays = eventDateList[1].split(","); // split the day from the month
+      eventDays.pop(); // Renove the last element, which is an empty string
     });
 }
 displayEventInfo();
