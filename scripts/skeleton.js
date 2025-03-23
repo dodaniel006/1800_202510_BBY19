79ac429b5b4a57ed12ae19e7a5005838def0f36e
Jazib <jazib.jeehan@gmail.com>
2025-03-23 13:33:32 -0700

    changed footer to mobile prioritize mobile screens

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -10,7 +10,7 @@ function loadSkeleton() {
             // User is signed in.
             // Do something for the user here.
             console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#footerPlaceholder').load('./text/footer-after-login.html'));
             console.log($('#enterCodePlaceholder').load('./text/enter-code-modal.html'));
             console.log($('#enterCodeButtonPlaceholder').load('./text/enter-code-button.html'));
             console.log("Logged In!");
