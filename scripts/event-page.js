087a3a421690c70c7ac4157bc134d901bd62c51f
DMartinez-Van <david.antonio.martinez@gmail.com>
2025-03-23 14:01:14 -0700

    rename createEvent to create-event, to match other file naming

--- scripts/event-page.js
+++ scripts/event-page.js
@@ -1,38 +1,42 @@
 let eventCodeToggle = false;
 
 function displayEventInfo() {
-	console.log("displayEvents");
-	let params = new URL(window.location.href);
-	let eventID = params.searchParams.get("docID");
-	console.log(eventID);
+  // Get eventID from params
+  let params = new URL(window.location.href);
+  let eventID = params.searchParams.get("docID");
 
-	db.collection("events").doc(eventID).get().then(doc => {
-		eventName = doc.data().name;
-		eventLocation = doc.data().location;
-		eventDate = doc.data().date;
-		eventTime = doc.data().time;
-		eventDescription = doc.data().description;
-		eventCode = doc.data().eventCode;
+  // Get event info from database
+  db.collection("events")
+    .doc(eventID)
+    .get()
+    .then((doc) => {
+      eventName = doc.data().name;
+      eventLocation = doc.data().location;
+      eventDate = doc.data().date;
+      eventTime = doc.data().time;
+      eventDescription = doc.data().description;
+      eventCode = doc.data().eventCode;
 
-		document.getElementById("event-title").innerHTML = eventName;
-		document.getElementById("event-date").innerHTML = "Date:	" + eventDate;
-		document.getElementById("event-time").innerHTML = "Time:	" + eventTime;
-		document.getElementById("event-location").innerHTML = "Location:	" + eventLocation;
-		document.getElementById("description").innerHTML = eventDescription;
-		document.getElementById("eventCode").style.visibility = "hidden";
-		document.getElementById("eventCode").innerHTML = eventCode;
-	})
+      // Populate html with info
+      document.getElementById("event-title").innerHTML = eventName;
+      document.getElementById("event-date").innerHTML = "Date:	" + eventDate;
+      document.getElementById("event-time").innerHTML = "Time:	" + eventTime;
+      document.getElementById("event-location").innerHTML =
+        "Location:	" + eventLocation;
+      document.getElementById("description").innerHTML = eventDescription;
+      document.getElementById("eventCode").style.visibility = "hidden";
+      document.getElementById("eventCode").innerHTML = eventCode;
+    });
 }
 displayEventInfo();
 
-document.getElementById("generateCodeButton").addEventListener(
-	"click",
-	function () {
-		eventCodeToggle = !eventCodeToggle;
-		if (eventCodeToggle) {
-			document.getElementById("eventCode").style.visibility = "visible";
-		} else {
-			document.getElementById("eventCode").style.visibility = "hidden";
-		}
-	},
-);
+document
+  .getElementById("generateCodeButton")
+  .addEventListener("click", function () {
+    eventCodeToggle = !eventCodeToggle;
+    if (eventCodeToggle) {
+      document.getElementById("eventCode").style.visibility = "visible";
+    } else {
+      document.getElementById("eventCode").style.visibility = "hidden";
+    }
+  });
