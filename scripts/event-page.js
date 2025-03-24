c5eb255f28abb4cb0acfe6e34eff34eed3fc124b
chellj <jjacques7@my.bcit.ca>
2025-03-23 19:03:07 -0700

    Add planner tools block to event-page that is only seen by event planner.

--- scripts/event-page.js
+++ scripts/event-page.js
@@ -15,7 +15,7 @@ function displayEventInfo() {
       eventDate = doc.data().date;
       eventTime = doc.data().time;
       eventDescription = doc.data().description;
-      eventCode = doc.data().eventCode;
+      
       selectedTime = doc.data().selectedTime;
 
       // Populate html with info
@@ -26,13 +26,15 @@ function displayEventInfo() {
       document.getElementById("event-location").innerHTML =
         "Location:	" + eventLocation;
       document.getElementById("description").innerHTML = eventDescription;
-      document.getElementById("eventCode").style.visibility = "hidden";
-      document.getElementById("eventCode").innerHTML = eventCode;
+	  showPlannerTools(doc);
 
       // select dates which match the eventDate list from the database and hightlight them
       let eventDateList = eventDate.split(" "); // split the date string into an array
       let eventDays = eventDateList[1].split(","); // split the day from the month
       eventDays.pop(); // Renove the last element, which is an empty string
+
+	  
+	
     });
 }
 displayEventInfo();
@@ -47,3 +49,26 @@ document
       document.getElementById("eventCode").style.visibility = "hidden";
     }
   });
+
+
+function showPlannerTools(doc){
+	firebase.auth().onAuthStateChanged((user) => {
+		if (user) {
+			eventPlanner = doc.data().hostId;
+			eventCode = doc.data().eventCode;
+			console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+			console.log("Event Planner " + eventPlanner);
+			if (user.uid == eventPlanner){
+				console.log($('#plannerTools').load('./text/invite-code.html'));
+				document.addEventListener('DOMContentLoaded', function() {
+					document.getElementById("eventCode").style.visibility = "hidden";
+      				document.getElementById("eventCode").innerHTML = eventCode;
+				});
+				
+			}
+			
+		} else {
+			console.log("No user is logged in."); // Log a message when no user is logged in
+		}
+	});
+}
